3d0049147448896348783dce83dc7bc5325a7102
codeLittleNewbie <935814110@qq.com>
2017-11-20 16:04:50 +0800

    模板修改完成

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,39 +1,63 @@
 <template>
   <div>
-    <group>
-      <cell title="title" value="value"></cell>
-    </group>
+    <datetime-view v-model="value1" ref="datetime" :format="format"></datetime-view>
+    <p class="info">2</p>
+    <div style="padding:15px;">
+      <x-button @click.native="changeValue('2017-11-11')" :disabled="format !== 'YYYY-MM-DD'" type="primary"> 1 </x-button>
+      <x-button @click.native="changeValue('2016-08-08')" :disabled="format !== 'YYYY-MM-DD'" type="primary"> 3 </x-button>
+      <x-button @click.native="toggleFormat" type="primary"> 4</x-button>
+      <x-button @click.native="showPopup = true" type="primary"> 4 </x-button>
+    </div>
+    <div v-transfer-dom>
+      <popup v-model="showPopup">
+        <datetime-view v-model="value2" format="YYYY-MM"></datetime-view>
+      </popup>
+    </div>
   </div>
 </template>
 
+
+
 <script>
-  import { Group, Cell } from 'vux'
+  import { DatetimeView, XButton, Popup, TransferDom } from 'vux'
 
   export default {
     components: {
-      Group,
-      Cell
+      DatetimeView,
+      XButton,
+      Popup
     },
-    mounted(){
-      console.log(this.$http.get);
+    directives: {
+      TransferDom
+    },
+    data () {
+      return {
+        value1: '2017-10-11',
+        value2: '2017-10',
+        showPopup: false,
+        format: 'YYYY-MM-DD'
+      }
+    },
+    methods: {
+      toggleFormat () {
+        if (this.format === 'YYYY-MM-DD') {
+          this.format = 'YYYY-MM-DD HH:mm'
+        } else {
+          this.format = 'YYYY-MM-DD'
+        }
+      },
+      changeValue (val) {
+        this.value1 = val
+        this.$refs.datetime.render()
+      }
     }
   }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .info {
+    padding-top: 15px;
+    text-align: center;
+    color: #666;
+  }
 </style>
